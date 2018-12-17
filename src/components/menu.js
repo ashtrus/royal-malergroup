521bdb32643c5459469980a66bfbf46d9643942b
ashtrus <zygimantas.butas@gmail.com>
2018-12-17 21:44:39 +0100

    menu component updated

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,34 +1,30 @@
 import React from 'react'
-import { Link } from "gatsby"
+import Container from 'react-bootstrap/lib/Container'
+import Navbar from 'react-bootstrap/lib/Navbar'
+import Nav from 'react-bootstrap/lib/Nav'
 
-const Menu = () => (
-  <div
-    style={{
-      background: '#f4f4f4',
-      paddingTop: '10px',
-    }}
-  >
-    <ul
-      style={{
-        listStyle: 'none',
-        display: 'flex',
-        justifyContent: 'space-evenly',
-      }}
-    >
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/about">About</Link>
-      </li>
-      <li>
-        <Link to="/services">Services</Link>
-      </li>
-      <li>
-        <Link to="/blog">Blog</Link>
-      </li>
-    </ul>
-  </div>
+const Menu = ({ siteTitle }) => (
+  <Container>
+    <Navbar collapseOnSelect expand="lg" className="text-uppercase py-4">
+      <Navbar.Brand
+        href="/"
+        style={{ fontSize: '1.6em', fontWeight: 'bold', color: 'royalblue' }}
+      >
+        {siteTitle}
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav
+          className="ml-auto"
+          style={{ fontSize: '0.8em', color: '#331a78' }}
+        >
+          <Nav.Link href="/about">About</Nav.Link>
+          <Nav.Link href="/services">Services</Nav.Link>
+          <Nav.Link href="/blog">Blog</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  </Container>
 )
 
 export default Menu
