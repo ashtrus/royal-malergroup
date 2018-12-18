8e0e1f0b8b18e46adbdc2617dca8a4170b02885b
ashtrus <zygimantas.butas@gmail.com>
2018-12-18 22:16:14 +0100

    unnecessary param removed

---
+++ src/components/services.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import Container from 'react-bootstrap/lib/container'
+import Row from 'react-bootstrap/lib/Row'
+import Col from 'react-bootstrap/lib/Col'
+import Card from 'react-bootstrap/lib/card'
+import CardGroup from 'react-bootstrap/lib/CardGroup'
+
+const Services = () => (
+  <>
+    <Container>
+      <h2 className="pt-4 pb-5 text-uppercase text-center">Our services</h2>
+
+      <Row className="py-4">
+        <Col md={{ span: 8, offset: 2 }}>
+          <p className="text-center">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores
+            nihil illum suscipit porro? Laudantium architecto cum quos rerum
+            repudiandae blanditiis reprehenderit explicabo. Quas nihil eum
+            tempore consectetur culpa voluptatibus doloremque!
+          </p>
+        </Col>
+      </Row>
+
+      <Row>
+        <CardGroup>
+          <Card>
+            <Card.Img variant="top" src="https://placeimg.com/320/240/arch" />
+            <Card.Body>
+              <Card.Title className="mb-3 text-center text-uppercase">
+                Service 1
+              </Card.Title>
+              <Card.Text>
+                This card has supporting text below as a natural lead-in to
+                additional content.{' '}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+
+          <Card>
+            <Card.Img variant="top" src="https://placeimg.com/320/240/arch" />
+            <Card.Body>
+              <Card.Title className="mb-3 text-center text-uppercase">
+                Service 2
+              </Card.Title>
+              <Card.Text>
+                This card has supporting text below as a natural lead-in to
+                additional content.{' '}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+
+          <Card>
+            <Card.Img variant="top" src="https://placeimg.com/320/240/arch" />
+            <Card.Body>
+              <Card.Title className="mb-3 text-center text-uppercase">
+                Service 3
+              </Card.Title>
+              <Card.Text>
+                This card has supporting text below as a natural lead-in to
+                additional content.{' '}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </CardGroup>
+      </Row>
+    </Container>
+  </>
+)
+
+export default Services
