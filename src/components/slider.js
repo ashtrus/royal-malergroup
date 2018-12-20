45bfa340bf7c7eca83670c08666d96f3471a28c2
ashtrus <zygimantas.butas@gmail.com>
2018-12-20 21:10:00 +0100

    slider updated

--- src/components/slider.js
+++ src/components/slider.js
@@ -6,36 +6,37 @@ const Slider = () => (
     <Carousel.Item>
       <img
         className="d-block w-100"
-        src="https://images.unsplash.com/photo-1526376043067-5af36c35cd6c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3000&q=80"
+        src="https://placeimg.com/1600/700/arch"
         alt="First slide"
       />
-      <Carousel.Caption>
-        <h3>First slide label</h3>
-        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+
+      <Carousel.Caption className="mb-5">
+        <h3 className="display-2">First slide label</h3>
+        <h4>Nulla vitae elit libero, a pharetra augue mollis interdum.</h4>
       </Carousel.Caption>
     </Carousel.Item>
 
     <Carousel.Item>
       <img
         className="d-block w-100"
-        src="https://images.unsplash.com/photo-1526376043067-5af36c35cd6c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3000&q=80"
+        src="https://placeimg.com/1600/700/arch"
         alt="Second slide"
       />
-      <Carousel.Caption>
-        <h3>Second slide label</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <Carousel.Caption className="mb-5">
+        <h3 className="display-2">Second slide label</h3>
+        <h4>Nulla vitae elit libero, a pharetra augue mollis interdum.</h4>
       </Carousel.Caption>
     </Carousel.Item>
 
     <Carousel.Item>
       <img
         className="d-block w-100"
-        src="https://images.unsplash.com/photo-1526376043067-5af36c35cd6c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3000&q=80"
+        src="https://placeimg.com/1600/700/arch"
         alt="Third slide"
       />
-      <Carousel.Caption>
-        <h3>Third slide label</h3>
-        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+      <Carousel.Caption className="mb-5">
+        <h3 className="display-2">Third slide label</h3>
+        <h4>Nulla vitae elit libero, a pharetra augue mollis interdum.</h4>
       </Carousel.Caption>
     </Carousel.Item>
   </Carousel>
