5e2f13749d56038d10e32cd1b3707ec9133d6eea
ashtrus <zygimantas.butas@gmail.com>
2018-12-21 22:19:11 +0100

    menu updated

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,30 +1,28 @@
 import React from 'react'
-import Container from 'react-bootstrap/lib/Container'
 import Navbar from 'react-bootstrap/lib/Navbar'
 import Nav from 'react-bootstrap/lib/Nav'
 
 const Menu = ({ siteTitle }) => (
-  <Container>
-    <Navbar collapseOnSelect expand="lg" className="text-uppercase py-4">
-      <Navbar.Brand
-        href="/"
-        style={{ fontSize: '1.6em', fontWeight: 'bold', color: 'royalblue' }}
-      >
-        {siteTitle}
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav
-          className="ml-auto"
-          style={{ fontSize: '0.8em', color: '#331a78' }}
-        >
-          <Nav.Link href="/about">About</Nav.Link>
-          <Nav.Link href="/services">Services</Nav.Link>
-          <Nav.Link href="/blog">Blog</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  </Container>
+  <Navbar
+    collapseOnSelect
+    expand="lg"
+    className="text-uppercase py-4 container"
+  >
+    <Navbar.Brand
+      href="/"
+      style={{ fontSize: '1.6em', fontWeight: 'bold', color: '#331a78' }}
+    >
+      {siteTitle}
+    </Navbar.Brand>
+    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+    <Navbar.Collapse id="responsive-navbar-nav">
+      <Nav className="ml-auto" style={{ fontSize: '0.8em', color: '#331a78' }}>
+        <Nav.Link href="/about">About</Nav.Link>
+        <Nav.Link href="/services">Services</Nav.Link>
+        <Nav.Link href="/blog">Blog</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
 )
 
 export default Menu
