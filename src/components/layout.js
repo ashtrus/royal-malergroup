a98f5404467a8cacef3b233cf13a941ffb26f637
ashtrus <zygimantas.butas@gmail.com>
2018-12-21 22:21:24 +0100

    layout updated. index page structured

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,47 +1,39 @@
 import React from 'react'
 import Helmet from 'react-helmet'
-import { StaticQuery, graphql } from "gatsby"
+import { StaticQuery, graphql } from 'gatsby'
 
-import Header from './header'
 import Menu from './menu'
 import './index.css'
 
 export default ({ children }) => (
-     <StaticQuery
-       query={graphql`
-         query LayoutQuery {
-           site {
-             siteMetadata {
-               title
-             }
-           }
-         }
-       `}
-       render={data => (
-         <>
-          <Helmet
-            title={data.site.siteMetadata.title}
-            meta={[
-              {
-                name: 'description',
-                content: 'This is a sample website for the Gatsby crash course',
-              },
-              { name: 'keywords', content: 'gatsby, react, tutorial' },
-            ]}
-          />
-          <Header siteTitle={data.site.siteMetadata.title} />
-          <Menu />
-          <div
-            style={{
-              margin: '0 auto',
-              maxWidth: 960,
-              padding: '0px 1.0875rem 1.45rem',
-              paddingTop: 0,
-            }}
-          >
-            {children}
-          </div>
-         </>
-       )}
-     />
+  <StaticQuery
+    query={graphql`
+      query LayoutQuery {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+    render={data => (
+      <>
+        <Helmet
+          title={data.site.siteMetadata.title}
+          meta={[
+            {
+              name: 'description',
+              content: 'Royal Malergroup homepage.',
+            },
+            {
+              name: 'keywords',
+              content: 'gatsby, react',
+            },
+          ]}
+        />
+        <Menu siteTitle={data.site.siteMetadata.title} />
+        {children}
+      </>
+    )}
+  />
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,23 @@
 import React from 'react'
-import Layout from "../components/layout"
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'react-bootstrap/dist/react-bootstrap.min.js';
+import Layout from '../components/layout'
+import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'react-bootstrap/dist/react-bootstrap.min.js'
+
+import Slider from '../components/slider'
+import Services from '../components/services'
+import ProjectList from '../components/projects'
+import AboutUs from '../components/about-us'
+import OurOffer from '../components/our-offer'
+import OurMaterials from '../components/our-materials'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Welcome to my website</h1>
-    <p>This is a sample Gatsby website.</p>
+    <Slider />
+    <Services />
+    <ProjectList />
+    <AboutUs />
+    <OurOffer />
+    <OurMaterials />
   </Layout>
 )
 
